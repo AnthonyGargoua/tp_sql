d22ab91e6140790bae3b19740352cb83794ea2ee
Anthony GARGOUA <56581894+AnthonyGargoua@users.noreply.github.com>
2026-07-09 16:58:42 +0200

    TP06 Mise à jour de données

---
+++ tp06_gargoua.sql
@@ -0,0 +1,23 @@
+SET SQL_SAFE_UPDATES=0;
+
+-- a. Mettez en minuscules la désignation de l’article dont l’identifiant est 2
+UPDATE ARTICLE a SET DESIGNATION = LOWER(a.DESIGNATION) WHERE ID=2;
+
+
+-- b. Mettez en majuscules les désignations de tous les articles dont le prix est strictement supérieur à 10€
+UPDATE ARTICLE a SET DESIGNATION = UPPER(a.DESIGNATION) WHERE a.PRIX > 10;
+
+
+-- c. Baissez de 10% le prix de tous les articles qui n’ont pas fait l’objet d’une commande. 
+
+
+-- d. Une erreur s’est glissée dans les commandes concernant Française d’imports. Les chiffres en base ne sont pas bons. Il faut doubler les quantités de tous les articles commandés à cette société.
+
+
+
+-- e. Mettez au point une requête update qui permette de supprimer les éléments entre parenthèses dans les désignations. Il vous faudra utiliser des fonctions comme substring et position.
+
+
+
+
+
